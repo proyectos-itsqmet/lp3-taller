f28d08f98ca5ff2fc848942549e8879ccc381073
Gerardo Araque <gerard.araq@gmail.com>
2026-06-22 09:29:32 -0500

    Inicio de proyecto

---
+++ Backend/src/test/java/com/lp3_taller/TallerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lp3_taller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
